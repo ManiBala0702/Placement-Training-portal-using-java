9860bbefe9684e62882e7c4a44a98cc93e9fce92
ManiBala0702 <manibala0702@gmail.com>
2025-06-14 12:02:30 +0530

    add symbol calculator program

---
+++ SymbolCalculator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class main{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+char a = scanner.next().charAt(0);
+int b = scanner.nextInt();
+int c = scanner.nextInt();
+int result = 0;
+switch(a){
+case'+':
+result = b+c;
+break;
+case'-':
+result = b-c;
+break;
+case'*':
+result = b*c;
+break;
+case'/':
+result = b/c;
+break;
+case'%':
+result = b%c;
+break;
+default:
+System.out.println("Invalid");
+return;}
+System.out.println(result);
+}}
